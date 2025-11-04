54d0aad367e219529b12ab3946c84749abc8a717
GustavoSFer <gustavo.sfernander@gmail.com>
2025-11-03 21:36:55 -0300

    feat: Iniciando o projeto

---
+++ src/main/java/com/fernandes/barbershop_agendamento_cabelereiro_api/BarbershopAgendamentoCabelereiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.fernandes.barbershop_agendamento_cabelereiro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopAgendamentoCabelereiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopAgendamentoCabelereiroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandes/barbershop_agendamento_cabelereiro_api/BarbershopAgendamentoCabelereiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandes.barbershop_agendamento_cabelereiro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopAgendamentoCabelereiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
