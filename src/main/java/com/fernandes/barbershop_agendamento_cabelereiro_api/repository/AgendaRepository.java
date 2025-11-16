1984a20776bcb6d574b4b7f5012d241ec1dcaaa3
GustavoSFer <gustavo.sfernander@gmail.com>
2025-11-15 22:43:36 -0300

    feat: Criado o repository de agenda

--- src/main/java/com/fernandes/barbershop_agendamento_cabelereiro_api/repository/AgendaRepository.java
+++ src/main/java/com/fernandes/barbershop_agendamento_cabelereiro_api/repository/AgendaRepository.java
@@ -1,6 +1,7 @@
 package com.fernandes.barbershop_agendamento_cabelereiro_api.repository;
 
 import com.fernandes.barbershop_agendamento_cabelereiro_api.entity.Agenda;
+import com.fernandes.barbershop_agendamento_cabelereiro_api.entity.Corte;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
