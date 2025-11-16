fbabc05b7b880de6f566e720eb33fc82f62e679c
GustavoSFer <gustavo.sfernander@gmail.com>
2025-11-15 23:35:59 -0300

    fix correção do create de um agendamento

--- src/main/java/com/fernandes/barbershop_agendamento_cabelereiro_api/service/AgendaImpl.java
+++ src/main/java/com/fernandes/barbershop_agendamento_cabelereiro_api/service/AgendaImpl.java
@@ -1,9 +1,15 @@
 package com.fernandes.barbershop_agendamento_cabelereiro_api.service;
 
 import com.fernandes.barbershop_agendamento_cabelereiro_api.entity.Agenda;
+import com.fernandes.barbershop_agendamento_cabelereiro_api.entity.Corte;
+import com.fernandes.barbershop_agendamento_cabelereiro_api.entity.Pessoa;
+import com.fernandes.barbershop_agendamento_cabelereiro_api.entity.Plano;
 import com.fernandes.barbershop_agendamento_cabelereiro_api.exception.AgendaException.AgendaNotFoundException;
 import com.fernandes.barbershop_agendamento_cabelereiro_api.repository.AgendaRepository;
 import com.fernandes.barbershop_agendamento_cabelereiro_api.service.interfaces.AgendaInterface;
+import com.fernandes.barbershop_agendamento_cabelereiro_api.service.interfaces.CorteInterface;
+import com.fernandes.barbershop_agendamento_cabelereiro_api.service.interfaces.PessoaInterface;
+import com.fernandes.barbershop_agendamento_cabelereiro_api.service.interfaces.PlanoInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +22,31 @@ public class AgendaImpl implements AgendaInterface {
     @Autowired
     private AgendaRepository agendaRepository;
 
+    @Autowired
+    private PessoaInterface pessoaInterface;
+
+    @Autowired
+    private CorteInterface corteInterface;
+
+    @Autowired
+    private PlanoInterface planoInterface;
+
     @Override
     public Agenda create(Agenda agenda) {
+        Pessoa pessoa = pessoaInterface.findById(agenda.getPessoa().getId());
+        Corte corte = null;
+        if (agenda.getCorte() != null && agenda.getCorte().getId() != null) {
+            corte = corteInterface.findById(agenda.getCorte().getId());
+        }
+
+        Plano plano = null;
+        if (agenda.getPlano() != null && agenda.getPlano().getId() != null) {
+            plano = planoInterface.findById(agenda.getPlano().getId());
+        }
+
+        agenda.setPessoa(pessoa);
+        agenda.setPlano(plano);
+        agenda.setCorte(corte);
         return agendaRepository.save(agenda);
     }
 
